58136c8867d5e49e4abafb9cddf95793063406be
Steven Ishiwara <info@akicreative.net>
2019-12-04 17:07:44 -0500

    Various Updates

--- src/Form.php
+++ src/Form.php
@@ -10,6 +10,352 @@ namespace akicreative\akiforms;
 class Form
 {
 
+	var $errors = [];
+	var $tabindex = 1;
+	var $echo = true;
+	var $csrf = true;
+	var $horizontal = false;
+	var $size = 'form-control-sm';
+	var $defaults = [];
+
+	private function parse($args, $arguments)
+	{
+
+		$arguments = array_merge($args, $arguments);
+
+		$attrs = [];
+
+		foreach($arguments as $key => $value){
+
+			if(!array_key_exists($key, $args)){
+
+				switch($key){
+
+					case "files":
+
+						if($value){
+
+							$key = 'enctype';
+							$value = 'multipart/form-data';
+					
+						}
+
+						break;
+
+					default: 
+
+						$value = '';
+						break;
+
+				}
+
+			}
+
+			if($value != ''){
+
+				$attrs[] = $key . '="' . $value . '"';
+			
+			}
+
+		}
+
+		return implode(' ', $attrs);
+
+	}
+
+	public function __construct($errors, $arguments = []){
+
+		foreach($arguments as $key => $value){
+
+			$this->$key = $value;
+		}
+
+		$this->errors = $errors;
+
+	}
+
+	public function open($arguments = [], $fill = false){
+
+		$args = [
+
+			'action' => '',
+			'id' => 'entryform',
+			'method' => 'POST'
+
+		];
+
+		echo '<form ' . $this->parse($args, $arguments) . '>';
+
+		echo csrf_field();
+
+		if($fill){
+
+			$array  = $fill->toArray();
+
+			foreach($array as $key => $value){
+
+				if(old($key)){
+
+					$array[$key] = old($key);
+				}
+			}
+		
+			$this->defaults = $array;
+
+		}
+
+		echo "\n";
+
+	}
+
+	public function close(){
+
+		echo '</form>' . "\n";
+
+	}
+
+	public function build($type, $label, $name, $cfgs = []){
+
+		$cfg = [
+
+			'fieldonly' => false,
+			'blockhelp' => '',
+			'echo' => $this->echo,
+			'size' => $this->size,
+			'class' => '',
+			'horizontal' => $this->horizontal,
+			'required' => false,
+			'default' => '',
+			'selectoptions' => [],
+			'selectblankfirst' => false,
+			'selectshortcut' => '',
+			'checkboxvalues' => [], // [[name, label, value]]
+			'testmode' => false,
+			'dateparams' => []
+
+		];
+
+		$attrs = [
+
+			'id' => strtolower($name),
+			'tabindex' => $this->tabindex,
+			'placeholder' => ''
+
+		];	
+
+		$this->tabindex++;
+
+		if(array_key_exists($name, $this->defaults)){
+
+			$cfg['default'] = $this->defaults[$name];
+		}
+
+		
+		$fieldattributes = ['name="' . $name . '"'];
+
+		foreach($cfgs as $key => $value){
+
+			if(array_key_exists($key, $attrs)){
+
+				$attrs[$key] = $value;
+
+			}
+
+			if(array_key_exists($key, $cfg)){
+
+				$cfg[$key] = $value;
+
+			}
+
+			if($key == 'attrs' && is_array($value)){
+
+				foreach($value as $akey => $avalue){
+
+					$fieldattributes[] = $akey . '="' . $avalue . '"';
+
+				}
+
+			}
+
+		}
+
+		if(old($name)){
+
+			$cfg['default'] = old($name);
+		}
+
+		foreach($attrs as $key => $value){
+
+			$fieldattributes[] = $key . '="' . $value . '"';
+		}
+
+		if(!$cfg['fieldonly']){
+
+			echo '<div class="form-group">';
+			echo '<label for="' .  $attrs['id'] . '">' . $label . '</label>';
+		}
+
+		$required = '';
+
+		if($cfg['required']){
+
+			$required = 'required';
+		}
+
+		$errorfeedback = '';
+
+		if($this->errors->has($name)){
+
+			$cfg['class'] .= ' is-invalid';
+
+			if(!$cfg['fieldonly']){
+
+				$errorfeedback = '<div class="invalid-feedback">
+		          ' . $this->errors->first($name) . '
+		        </div>';
+
+	    	}
+		}
+
+		switch($type){
+
+			case 'text':
+			case 'email':
+
+				
+				echo '<input type="' . $type . '" class="form-control ' . $this->size . ' ' . $cfg['class'] . '" ' . implode(' ', $fieldattributes) . ' aria-describedby="' .  $attrs['id'] . 'Help" ' . $required . ' value="' . $cfg['default'] . '">';
+    
+				echo $errorfeedback;
+
+				break;
+
+			case 'textarea':
+
+				echo '<textarea class="form-control ' . $this->size . ' ' . $cfg['class'] . '" ' . implode(' ', $fieldattributes) . ' aria-describedby="' .  $attrs['id'] . 'Help" ' . $required . '>' . $cfg['default'] . '</textarea>';
+    
+				echo $errorfeedback;
+
+				break;
+
+			case 'select':
+
+				switch($cfg['selectshortcut']){
+
+					case "yesno":
+						$cfg['selectoptions'] = [1 => 'Yes', 0 => 'No'];
+						break;
+
+					case "noyes":
+						$cfg['selectoptions'] = [0 => 'No', 0 => 'Yes'];
+						break;
+
+				}
+
+				echo '<select class="form-control ' . $this->size . ' ' . $cfg['class'] . '" ' . implode(' ', $fieldattributes) . ' aria-describedby="' .  $attrs['id'] . 'Help" ' . $required . '>';
+
+				
+
+				if($cfg['selectblankfirst'] != ''){
+
+					echo '<option value="">' . $cfg['selectblankfirst'] . '</option>';
+
+				}
+
+				foreach($cfg['selectoptions'] as $key => $option){
+
+					$selected = '';
+
+					if($key == $cfg['default']){
+
+						$selected = 'selected';
+					}
+
+					echo '<option value="' . $key . '" ' . $selected . '>' . $option . '</option>';
+
+				}
+
+				echo '</select>';
+    
+				echo $errorfeedback;
+
+				break;
+
+			case 'dateselect':
+
+				echo dateselect($name, $cfg['dateparams'] + ['default' => $cfg['default']]);
+
+				break;
+
+			case 'checkbox':
+			case 'checkbox-inline':
+
+				
+
+				$array = $cfg['checkboxvalues'];
+
+				foreach($array as $a){
+
+					if($type == 'checkbox-inline'){
+
+						echo '<div class="form-check form-check-inline">';
+
+					}else{
+
+						echo '<div class="form-check">';
+					}
+
+					$value = 1;
+
+					if(array_key_exists(2, $a)){
+
+						$value = $a[2];
+					}
+
+					echo '<input class="form-check-input" type="checkbox" name="' . $name . '" id="' . $name . $a[0] . '" value="' . $value . '">
+  					<label class="form-check-label" for="' . $name . $a[0] . '">' . $a[1] . '</label>';
+
+  					echo '</div>';
+
+  				}
+
+  				
+
+				break;
+
+
+		}
+
+		if(!$cfg['fieldonly']){
+
+			if($cfg['blockhelp'] != ''){
+
+				echo '<small id="' . $attrs['id'] . 'Help" class="form-text text-muted">' . $cfg['blockhelp'] . '</small>';
+
+			}
+
+			echo '</div>';
+		}
+
+		if($cfg['testmode']){
+
+			echo "<pre>";
+			print_r($cfg);
+			echo "</pre>";
+
+			echo "<pre>";
+			print_r($attrs);
+			echo "</pre>";
+
+			echo "<pre>";
+			print_r($attrs);
+			echo "</pre>";		
+
+		}
+
+
+	}
+
+
 	public function test($val)
 	{
 
@@ -20,6 +366,7 @@ class Form
 		echo $val;
 
 	}
+
 
 }
 
--- src/helpers.php
+++ src/helpers.php
@@ -84,3 +84,540 @@ if (! function_exists('akiformsstates')) {
 		];
     }
 }
+
+if (! function_exists('formatphone')) {
+
+    function formatphone($text, $version = 'dash') {
+        // ...
+
+       $text = preg_replace("![^0-9]+!", "", $text);
+
+       if($text == ''){
+       		return $text;
+       }
+
+       $text = preg_replace("!^1!", "", $text);
+
+       $num1 = substr($text, 0, 3);
+    
+       $num2 = substr($text, 3, 3);
+
+       $num3 = substr($text, 6, 4);
+
+       $ext = substr($text, 10);
+
+       if($version == 'dash'){
+
+       		$return = $num1 . '-' . $num2 . '-' . $num3;
+
+       		if($ext != ''){
+
+       			$return = $return . ' ext ' . $ext;
+       		}
+       }else{
+
+       		$return = '(' .$num1 . ') ' . $num2 . '-' . $num3;
+
+       		if($ext != ''){
+
+       			$return = $return . ' ext ' . $ext;
+       		}
+       }
+
+       return $return;
+
+    }
+}
+
+if (! function_exists('timearray')) {
+
+    function timearray($minutes = 15) {
+      
+        $return = [];
+
+        for($i = 0; $i <= 23; $i++){
+
+            switch($minutes){
+
+                case "15":
+                    $minutes = ["00", 15, 30, 45];
+                    break;
+                case "30":
+                    $minutes = ["00", 30];
+                    break;
+            }
+
+            foreach($minutes as $m){
+
+                $label = date("g:ia", strtotime("$i:$m"));
+
+                $key = date("H:i:00", strtotime("$i:$m"));
+
+                $return[$key] = $label;
+
+            }
+
+        }
+
+        return $return;
+    }
+}
+
+if (! function_exists('timeselect')) {
+
+    function timeselect($name = 'time', $default = '', $params = []) {
+
+        $above = '';
+        $class = '';
+        $style = 'width: auto;';
+        $timeminutes = 15;
+
+        extract($params);
+      
+        $values = timearray($timeminutes);
+
+        $hour = date("H", strtotime($default));
+
+        $minute = date("i", strtotime($default));
+
+        $time = $hour . ":";
+
+        if($minute == 0){
+
+        	$time = $time . "00";
+
+        }elseif($minute <= 15 && $minute > 0){
+
+            if($timeminutes == 15){
+
+                $time = $time . 15;
+
+            }else{
+
+                $time = $time . 30;
+            }
+        
+        }elseif($minute <= 30 && $minute > 0){
+
+            $time = $time . 30;
+
+        }elseif($minute <= 45 && $minute > 0){
+
+            if($timeminutes == 15){
+
+                $time = $time . 45;
+
+            }else{
+
+                $time = $hour + 1 . ":00";
+            }
+
+        }else{
+
+            $time = $hour + 1 . ":00";
+        }
+
+        $default = date("H:i:00", strtotime($time));
+
+        ob_start();
+
+        echo '<select class="form-control" id="' . $name . '" name="' . $name . '" type="select" style="' . $style . '">';
+
+        echo $above;
+
+
+
+        foreach($values as $key => $value){
+
+            $selected = '';
+
+            if($key == $default){
+
+                $selected = 'selected';
+            }
+
+            echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>' . "\n";
+
+        }
+
+        echo '</select>';
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}
+
+if (! function_exists('monthselect')) {
+
+    function monthselect($name = 'month', $default = '', $params = []) {
+
+        $above = '';
+        $class = '';
+        $style = 'width: auto;';
+
+        extract($params);
+      
+        $values = montharray();
+
+        ob_start();
+
+        echo '<select class="form-control" id="' . $name . '" name="' . $name . '" type="select" style="' . $style . '">';
+
+        echo $above;
+
+        foreach($values as $key => $value){
+
+            $selected = '';
+
+            if($key == $default){
+
+                $selected = 'selected';
+            }
+
+            echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>' . "\n";
+
+        }
+
+        echo '</select>';
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}
+
+if (! function_exists('montharray')) {
+
+    function montharray($datecode = "F") {
+      
+        $return = [];
+
+        for($i = 1; $i <= 12; $i++){
+
+            $key = date("m", strtotime("2000-" . $i . "-01"));
+            $label = date("F", strtotime("2000-" . $i . "-01"));
+
+            $return[$key] = $label;
+
+        }
+
+        return $return;
+    }
+}
+
+if (! function_exists('monthselect')) {
+
+    function monthselect($name = 'month', $default = '', $params = []) {
+
+        $above = '';
+        $class = '';
+        $style = 'width: auto;';
+
+        extract($params);
+      
+        $values = montharray();
+
+        ob_start();
+
+        echo '<select class="form-control" id="' . $name . '" name="' . $name . '" type="select" style="' . $style . '">';
+
+        echo $above;
+
+        foreach($values as $key => $value){
+
+            $selected = '';
+
+            if($key == $default){
+
+                $selected = 'selected';
+            }
+
+            echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>' . "\n";
+
+        }
+
+        echo '</select>';
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}
+
+if (! function_exists('dayarray')) {
+
+    function dayarray() {
+      
+        $return = [];
+
+        for($i = 1; $i <= 31; $i++){
+
+            $key = date("d", strtotime("2000-01-" . $i));
+            $label = date("j", strtotime("2000-01-" . $i));
+
+            $return[$key] = $label;
+
+        }
+
+        return $return;
+    }
+}
+
+if (! function_exists('dayselect')) {
+
+    function dayselect($name = 'day', $default = '', $params = []) {
+
+        $above = '';
+        $class = '';
+        $style = 'width: auto;';
+
+        extract($params);
+      
+        $values = dayarray();
+
+        ob_start();
+
+        echo '<select class="form-control" id="' . $name . '" name="' . $name . '" type="select" style="' . $style . '">';
+
+        echo $above;
+
+        foreach($values as $key => $value){
+
+            $selected = '';
+
+            if($key == $default){
+
+                $selected = 'selected';
+            }
+
+            echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>' . "\n";
+
+        }
+
+        echo '</select>';
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}
+
+if (! function_exists('yeararray')) {
+
+    function yeararray($start = '', $end = '') {
+      
+        if($start == ''){
+
+            $start = date("Y");
+        }
+
+        if($end == ''){
+
+            $end = date("Y") + 5;
+        }     
+
+        $return = [];
+
+        if($end >= $start){
+
+            for($i = $start; $i <= $end; $i++){
+
+                $key = $i;
+
+                $return[$i] = $i;
+
+            }
+
+        }else{
+
+            for($i = $end; $i >= $start; $i--){
+
+                $key = $i;
+
+                $return[$i] = $i;
+
+            }
+        }
+
+        return $return;
+    }
+}
+
+if (! function_exists('yearselect')) {
+
+    function yearselect($name = 'year', $default = '', $params = []) {
+
+        $above = '';
+        $class = '';
+        $style = 'width: auto;';
+        $start = '';
+        $end = '';
+
+        extract($params);
+      
+        $values = yeararray($start, $end);
+
+        ob_start();
+
+        echo '<select class="form-control" id="' . $name . '" name="' . $name . '" type="select" style="' . $style . '">';
+
+        echo $above;
+
+        foreach($values as $key => $value){
+
+            $selected = '';
+
+            if($key == $default){
+
+                $selected = 'selected';
+            }
+
+            echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>' . "\n";
+
+        }
+
+        echo '</select>';
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}
+
+if (! function_exists('dateselect')) {
+
+    function dateselect($prefix, $params = []) {
+
+        $show = ['month', 'day', 'year'];
+
+        $monthparams = [];
+        $dayparams = [];
+        $yearparams = [];
+
+        $timeminutes = 15;
+        $timename = 'time';
+
+        $default = date("Y-m-d H:i:00");
+
+        $month = '';
+        $day = '';
+        $year = '';
+
+        $label = '';
+
+        extract($params);
+
+        if($default != ''){
+
+            $month = date("m", strtotime($default));
+            $day = date("d", strtotime($default));
+            $year = date("Y", strtotime($default));
+        }
+
+        ob_start();
+
+        if($label != ''){
+
+            echo '<div class="form-group">';
+            echo '<label>' . $label . '</label>';
+
+        }
+
+        echo '<ul class="list-inline mb-0">';
+
+        if(in_array('month', $show)){
+
+            echo '<li class="list-inline-item">';
+
+            echo monthselect($prefix . 'month', $month, $monthparams);
+
+            echo '</li>';
+
+        }
+
+        if(in_array('day', $show)){
+
+            echo '<li class="list-inline-item">';
+
+            echo dayselect($prefix . 'day', $day, $dayparams);
+
+            echo '</li>';
+
+        }
+
+        if(in_array('year', $show)){
+
+            echo '<li class="list-inline-item">';
+
+            echo yearselect($prefix . 'year', $year, $yearparams);
+
+            echo '</li>';
+
+        }
+
+        if(in_array('time', $show)){
+
+            echo '<li class="list-inline-item">';
+
+            echo timeselect($prefix . 'time', $default, ['minutes' => $timeminutes]);
+
+            echo '</li>';
+        }
+
+
+        echo '</ul>';
+
+        if($label != ''){
+
+            echo '</div>';
+
+        }
+
+        $return = ob_get_contents();
+
+        ob_get_clean();
+
+        return $return;
+
+    }
+
+}
+
+if (! function_exists('datesql')) {
+
+    function datesql($prefix, $params = []) {
+
+        $show = ['month', 'day', 'year'];
+
+        extract($params);
+
+        $month = request()->input($prefix . 'month', '');
+        $day = request()->input($prefix . 'day', '');
+        $year = request()->input($prefix . 'year', '');
+
+        if(in_array('time', $show)){
+
+            $time = request()->input($prefix . 'time', '00:00:00');
+
+            $return = date("Y-m-d H:i:00", strtotime($year . '-' . $month . '-' . $day . ' ' . $time));
+
+        }else{
+
+            $return = date("Y-m-d", strtotime($year . '-' . $month . '-' . $day));
+        }
+
+        return $return;
+    }
+
+}
