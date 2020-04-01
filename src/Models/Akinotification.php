560f55550306b19e33ee2256f1865420f54d54fb
Steven Ishiwara <info@akicreative.net>
2020-04-01 17:47:09 -0400

    Create Class

--- src/Models/Akinotification.php
+++ src/Models/Akinotification.php
@@ -3,10 +3,48 @@
 namespace AkiCreative\AkiForms\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
+use MailQueue;
 
 class Akinotification extends Model
 {
     
-	
+	static public function process()
+	{
+
+		$rows = Akinotification::whereNull('sent_at')->where('send_at', '<=', now())->take(25)->get();
+
+        foreach($rows as $row){
+
+
+            if($row->notificationtype == 'email'){
+
+                if($row->email_toname == ''){
+
+                    $row->email_toname = $row->email_toemail;
+                }
+
+                if($row->email_test_flg){
+
+                    $row->email_subject = $row->email_subject . ' - ORIG TO: ' . $row->email_toemail;
+
+                    $row->email_toemail = 'steven.i@me.com';
+                }
+
+                Mail::send(new MailQueue($row)); 
+
+                $row->sent_at = now();
+                $row->save();
+   
+
+            }elseif($row->notificationtype == 'telegram'){
+
+
+
+            }
+
+        }
+
+	}
 
 }
--- src/notifications.php
+++ src/notifications.php
@@ -4,38 +4,6 @@ if (! function_exists('akinotificationqueue')) {
 
     function akinotificationqueue() {
         
-        $rows = AkiCreative\AkiForms\Models\Akinotification::whereNull('sent_at')->where('send_at', '<=', now())->take(25)->get();
-
-        foreach($rows as $row){
-
-
-            if($row->notificationtype == 'email'){
-
-                if($row->email_toname == ''){
-
-                    $row->email_toname = $row->email_toemail;
-                }
-
-                if($row->email_test_flg){
-
-                    $row->email_subject = $row->email_subject . ' - ORIG TO: ' . $row->email_toemail;
-
-                    $row->email_toemail = 'steven.i@me.com';
-                }
-
-                \Illuminate\Support\Facades\Mail::send(new \AkiCreative\AkiForms\Models\MailQueue($row)); 
-
-                $row->sent_at = now();
-                $row->save();
-   
-
-            }elseif($row->notificationtype == 'telegram'){
-
-
-
-            }
-
-        }
-
+        
     }
 }
