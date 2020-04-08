99ce632d4520d00be42cfffeafd16076b35f00b9
Steven Ishiwara <info@akicreative.net>
2020-04-08 14:59:58 -0400

    Update Form.php

--- src/Form.php
+++ src/Form.php
@@ -621,8 +621,6 @@ class Form
 
 					echo '<div class="input-group">';
 
-					echo $cfg['default'] . '---';
-
 					echo '<input type="' . $type . '" class="akidpdisplay form-control ' . $this->size . ' ' . $cfg['class'] . '" ' . implode(' ', $fieldattributes) . ' aria-describedby="' .  $attrs['id'] . 'Help" ' . $required . ' value="' . outdate($cfg['default'], $dpcfg['datepickerformat']) . '" style="background-color: #FFF;" data-target="' . $datepickername .'" readonly>';
 
 					echo '<input type="hidden" name="' . $datepickername . '" id="' . $datepickername . '" value="' . $cfg['default'] . '"';
