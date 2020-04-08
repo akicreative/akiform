10833ac45893f869d432d4819b65c681bd6df9a5
Steven Ishiwara <info@akicreative.net>
2020-04-08 00:12:42 -0400

    Update Form.php

--- src/Form.php
+++ src/Form.php
@@ -917,7 +917,7 @@ echo <<<EOT
 
 			<!-- Modal -->
 			<div class="modal" id="akidatepickermodal" tabindex="-1" role="dialog" aria-labelledby="akidatepickerTitle" aria-hidden="true">
-			  <div class="modal-dialog modal-dialog-centered" role="document">
+			  <div class="modal-dialog" role="document">
 			    <div class="modal-content">
 			      <div class="modal-header">
 			        <h5 class="modal-title" id="akidatepickerTitle"></h5>
