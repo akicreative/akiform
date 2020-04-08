69abcc77365d9cb7e6a4907cd71e7c93187ec009
Steven Ishiwara <info@akicreative.net>
2020-04-07 22:39:33 -0400

    Update calendar.blade.php

--- resources/views/datepicker/calendar.blade.php
+++ resources/views/datepicker/calendar.blade.php
@@ -1,11 +1,5 @@
 <?
 
-
-
-echo "<pre>";
-print_r(request()->all());
-echo "</pre>";	
-
 $akidp = new AkiForm($errors);
 
 $akidp->fill(collect(['akidpmonth' => $currentmonth, 'akidpyear' => $currentyear]));
