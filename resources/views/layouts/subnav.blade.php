8b4909a5b9f5d384a84e8b4671d1ce6e8274d499
Steven Ishiwara <info@akicreative.net>
2020-05-11 17:48:41 -0400

    Add Nav Views

---
+++ resources/views/layouts/subnav.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="{{ $akisubnavurl ?? '' }}">{{ $akisubnavtitle ?? '' }}</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+    	@isset($akisubnav)
+
+    		@foreach($akisubnav as $akinav)
+
+    			{!! $akinav !!}
+
+
+    		@endforeach
+
+
+    	@endisset
+
+  
+    </ul>
+   
+  </div>
+</nav>
