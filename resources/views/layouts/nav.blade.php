604241c547991c81effd67cf71ad1e960c0d844c
Steven Ishiwara <info@akicreative.net>
2020-05-12 17:41:47 -0400

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -22,16 +22,6 @@
     	@endisset
 
 
-    	<li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         MORE
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="{{ route('textblocks.index') }}">Text Blocks</a>
-        </div>
-      </li>
-
-
       <? /*
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
