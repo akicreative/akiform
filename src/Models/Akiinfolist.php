124aaed2286c957f35ae329179c0b16c0c73f998
Steven Ishiwara <info@akicreative.net>
2020-11-22 16:38:11 -0500

    Update Akiinfolist.php

--- src/Models/Akiinfolist.php
+++ src/Models/Akiinfolist.php
@@ -5,7 +5,7 @@ namespace AkiCreative\AkiForms\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Infolist extends Model
+class Akiinfolist extends Model
 {
 
 	protected $table = 'akiform_infolists';
