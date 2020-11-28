d4ad51b3410aa5a0b9fe7396c7910eec5ac0f6c9
Steven Ishiwara <info@akicreative.net>
2020-11-27 22:16:43 -0500

    Create 2020_11_28_030903_update_akiformakiassetssquare_table.php

---
+++ database/migrations/2020_11_28_030903_update_akiformakiassetssquare_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateAkiformakiassetssquareTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('akiform_assets', function (Blueprint $table) {
+            $table->string('serverfilenamesq')->nullable()->after('serverfilenametn');
+            
+        });
+
+        Schema::table('akiform_categories', function (Blueprint $table) {
+            $table->integer('assetsqw')->default(400);
+            $table->integer('assetsqh')->default(400);
+            $table->integer('assettnw')->default(400)->change();
+            $table->integer('assettnh')->default(400)->change();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
