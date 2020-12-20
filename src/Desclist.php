617a8c125444943f472f85c7c21f1543a43dd024
Steven Ishiwara <info@akicreative.net>
2020-12-20 16:15:38 -0500

    Update Desclist.php

--- src/Desclist.php
+++ src/Desclist.php
@@ -13,6 +13,8 @@ class Desclist
 	var $type = 'desclist';
 	var $dtclass = 'col-12 col-md-4';
 	var $ddclass = 'col-12 col-md-8';
+	var $trueicon = '<i class="fa fa-check text-success fa-lg"></a>';
+	var $falseicon = '';
 
 	public function open($class = ''){
 
@@ -54,6 +56,30 @@ class Desclist
 			echo '<dt class="' . $this->dtclass . ' ' . $dtclass . '">' . $dt . '</dt>';
 
         	echo '<dd class="' . $this->ddclass . ' '. $ddclass . '">' . $dd . '</dd>'; 
+
+    	}
+
+	}
+
+	public function bool($value, $dd = '', $dtclass = 'col-1', $ddclass = 'col-11'){
+
+		if($value){
+
+			$icon = $this->trueicon;
+		}else{
+
+			$icon = $this->falseicon;
+		}
+
+		if($this->type == 'listgroup'){
+
+			echo '<div class="list-group-item"><div class="row"><div class="' . $dtclass . '">' . $icon . '</div><div class="' . $ddclass . '">' . $dd . '</div></div></div>';
+
+		}else{
+
+			echo '<dt class="' . $dtclass . '">' . $icon . '</dt>';
+
+        	echo '<dd class="' . $ddclass . '">' . $dd . '</dd>'; 
 
     	}
 
