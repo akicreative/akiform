918f435e6b4a346196408771fab08747311c4c10
Steven Ishiwara <info@akicreative.net>
2021-01-05 15:09:50 -0500

    Update subnav.blade.php

--- resources/views/layouts/subnav.blade.php
+++ resources/views/layouts/subnav.blade.php
@@ -1,4 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+
+  <div class="container-fluid">
+
   <a class="navbar-brand" href="{{ $akisubnavurl ?? '' }}">{{ $akisubnavtitle ?? '' }}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -29,4 +32,6 @@
       @endisset
    
   </div>
+
+</div>
 </nav>
