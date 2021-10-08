e453054c1457e617a35559374d7367da309afbac
Steven Ishiwara <info@akicreative.net>
2021-10-08 09:20:35 -0400

    Update 2021_10_07_102317_update_akiformakiassetssubcategory_table.php

--- database/migrations/2021_10_07_102317_update_akiformakiassetssubcategory_table.php
+++ database/migrations/2021_10_07_102317_update_akiformakiassetssubcategory_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateAkiformakiassetsubcategoryTable extends Migration
+class UpdateAkiformakiassetssubcategoryTable extends Migration
 {
     /**
      * Run the migrations.
