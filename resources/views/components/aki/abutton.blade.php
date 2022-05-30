bb8b65aa90ec157b6d5109703bdc69b7ccea93fd
Steven Ishiwara <info@akicreative.net>
2022-05-30 15:09:58 -0400

    Update

--- resources/views/components/aki/abutton.blade.php
+++ resources/views/components/aki/abutton.blade.php
@@ -1,17 +1,39 @@
 @props([
 
 	'template' => 'primary',
-	'href' => '#'
+	'href' => '#',
+	'size' => 'base'
 
 ])
 
+<?
+
+$xpadding = 'px-4';
+
+switch($size){
+
+    case "slim": 
+        $ypadding = 'py-1';
+        $xpadding = 'px-2';
+        break;
+    case "sm": 
+        $ypadding = 'py-1';
+        break;
+    default:
+        $ypadding = 'py-2';
+        break;
+
+}
+
+?>
+
 @if($template == 'create')
 
-<a href="{{ $href }}" {{ $attributes }} class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">{{ $slot }}</a>
+<a href="{{ $href }}" {{ $attributes }} class="{{ $xpadding }} {{ $ypadding }} inline-flex items-center border border-transparent text-xs font-medium rounded shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">{{ $slot }}</a>
 
 @else
 
-<a href="{{ $href }}" {{ $attributes }} class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ $slot }}</a>
+<a href="{{ $href }}" {{ $attributes }} class="{{ $xpadding }} {{ $ypadding }} inline-flex items-center border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ $slot }}</a>
 
 
 @endif
