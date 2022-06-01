b2c263068be628e32857427cd3626564e9173260
Steven Ishiwara <info@akicreative.net>
2022-06-01 15:51:29 -0400

    Update abutton.blade.php

--- resources/views/components/aki/abutton.blade.php
+++ resources/views/components/aki/abutton.blade.php
@@ -29,11 +29,11 @@ switch($size){
 
 @if($template == 'create')
 
-<a href="{{ $href }}" {{ $attributes }} class="{{ $xpadding }} {{ $ypadding }} inline-flex items-center border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">{{ $slot }}</a>
+<a href="{{ $href }}" {{ $attributes }} class="{{ $xpadding }} {{ $ypadding }} inline-flex items-center border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">{!! $slot !!}</a>
 
 @else
 
-<a href="{{ $href }}" {{ $attributes }} class="{{ $xpadding }} {{ $ypadding }} inline-flex items-center border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ $slot }}</a>
+<a href="{{ $href }}" {{ $attributes }} class="{{ $xpadding }} {{ $ypadding }} inline-flex items-center border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{!! $slot !!}</a>
 
 
 @endif
