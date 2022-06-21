1fc5b7d1329632daf547c885c98f7c7aab30b831
Steven Ishiwara <info@akicreative.net>
2022-06-21 14:27:13 -0400

    Update group.blade.php

--- resources/views/components/aki/group.blade.php
+++ resources/views/components/aki/group.blade.php
@@ -33,6 +33,13 @@
 		<div class="sm:grid sm:grid-cols-{{ $cols }} sm:gap-4 sm:items-start sm:pt-0">
 			<label for="{{ $for }}" class="block text-sm sm:mt-px sm:pt-2 {{ $labelclass }}"> {{ $label }} </label>
 	  		<div class="mt-1 sm:mt-0 sm:col-span-{{ $span }}">
+
+@elseif($display == 'horizontaltightlines')
+  	
+  	<div class="space-y-0 sm:space-y-0">
+		<div class="sm:grid sm:grid-cols-{{ $cols }} sm:gap-4 sm:items-start sm:pt-0">
+			<label for="{{ $for }}" class="block text-sm sm:mt-px sm:pt-3 border-top border-bottom {{ $labelclass }}"> {{ $label }} </label>
+	  		<div class="mt-1 sm:mt-0 sm:col-span-{{ $span }}">
 @endif
 
  {{ $slot }}
