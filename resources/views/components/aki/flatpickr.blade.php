1f85fec74c2ada5b89794c8aaf7fe45605e9db89
Steven Ishiwara <info@akicreative.net>
2022-06-21 15:35:27 -0400

    Update flatpickr.blade.php

--- resources/views/components/aki/flatpickr.blade.php
+++ resources/views/components/aki/flatpickr.blade.php
@@ -1,6 +1,5 @@
 @props([
-    'options' => [],
-    'class' => 'w-24'
+    'options' => []
 ])
 
 @php
@@ -12,7 +11,7 @@
                     ], $options);
 @endphp
 
-<div class="mt-1 relative rounded-md shadow-sm" wire:ignore>
+<div class="mt-1 relative rounded-md shadow-sm w-24" wire:ignore>
 
     <input
         x-data="{
@@ -30,7 +29,7 @@
     />
 
     <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-      <span class="text-gray-500 sm:text-sm"> <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <span class="text-gray-500 sm:text-sm"> <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
 </svg> </span>
     </div>
