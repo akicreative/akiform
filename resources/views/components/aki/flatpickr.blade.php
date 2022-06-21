7d651c680e20839e4575a42702190e79a4f82603
Steven Ishiwara <info@akicreative.net>
2022-06-21 15:32:09 -0400

    Update flatpickr.blade.php

--- resources/views/components/aki/flatpickr.blade.php
+++ resources/views/components/aki/flatpickr.blade.php
@@ -1,4 +1,7 @@
-@props(['options' => []])
+@props([
+    'options' => [],
+    'class' => 'w-24'
+])
 
 @php
     $options = array_merge([
@@ -9,7 +12,8 @@
                     ], $options);
 @endphp
 
-<div wire:ignore>
+<div class="mt-1 relative rounded-md shadow-sm" wire:ignore>
+
     <input
         x-data="{
              value: @entangle($attributes->wire('model')), 
@@ -22,6 +26,13 @@
         x-ref="input"
         x-bind:value="value"
         type="text"
-        {{ $attributes->merge(['class' => 'bg-white mt-1 block shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md']) }}
+        {{ $attributes->merge(['class' => 'bg-white block  focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 ']) }}
     />
+
+    <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+      <span class="text-gray-500 sm:text-sm"> <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+</svg> </span>
+    </div>
+
 </div>
