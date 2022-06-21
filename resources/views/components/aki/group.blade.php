292b8fd81a7f36578154c0538a689a4a884d58de
Steven Ishiwara <info@akicreative.net>
2022-06-21 14:32:49 -0400

    Update group.blade.php

--- resources/views/components/aki/group.blade.php
+++ resources/views/components/aki/group.blade.php
@@ -38,7 +38,7 @@
   	
   	<div class="space-y-0 sm:space-y-0">
 		<div class="sm:grid sm:grid-cols-{{ $cols }} sm:gap-4 sm:items-start sm:pt-0">
-			<label for="{{ $for }}" class="block text-sm sm:mt-px sm:py-3 md:border-top md:border-bottom {{ $labelclass }}"> {{ $label }} </label>
+			<label for="{{ $for }}" class="block text-sm sm:mt-px sm:py-3 sm:border-top sm:border-bottom {{ $labelclass }}"> {{ $label }} </label>
 	  		<div class="mt-1 sm:mt-0 sm:col-span-{{ $span }}">
 @endif
 
