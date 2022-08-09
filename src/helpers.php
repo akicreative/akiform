e6eae7d3bc25373be2c55bdcf2502d837f22692d
Steven Ishiwara <info@akicreative.net>
2022-08-09 08:53:02 -0400

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -132,7 +132,7 @@ if (! function_exists('formatphone')) {
 
        if($version == 'clean'){
 
-            if(substr($text, 0, 1) === 1{
+            if(substr($text, 0, 1) === 1){
 
                 $text = substr($text, 1);
             }
