25c7e5116a783f0286db73db6f0cd56cb3a54799
Steven Ishiwara <info@akicreative.net>
2022-09-20 11:01:40 -0400

    Update base.blade.php

--- resources/views/components/aki/button/base.blade.php
+++ resources/views/components/aki/button/base.blade.php
@@ -46,7 +46,7 @@
 
 @else
 
-<a href="{{ $href }}" 
+<a href="{!! $href !!}" 
 {{ $attributes->merge([
     'class' => $ypadding .  ' ' . $xpadding . ' border rounded-md text-sm leading-5 font-medium focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition duration-150 ease-in-out hover:no-underline' . ($attributes->get('disabled') ? ' opacity-75 cursor-not-allowed' : ''),
 ]) }}
