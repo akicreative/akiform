f9fe56842b1203f0c63b9dc085fc56f8260984bc
Steven Ishiwara <info@akicreative.net>
2023-07-30 11:51:17 -0400

    Update

--- src/telegram.php
+++ src/telegram.php
@@ -1,10 +1,191 @@
 <?php
 
-if (! function_exists('akiformstelegramsend')) {
+if (! function_exists('telegramMessage')) {
 
-    function akiformstelegramsend() {
-        
-        
+    function telegramMessage($chat_id, $message, $params = []){
+
+        $url = 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/sendMessage';
+
+        $params['chat_id'] = $chat_id;
+        $params['text'] = urldecode($message);
+        $params['parse_mode'] = 'Markdown';
+
+
+        if(array_key_exists('buttons', $params)){
+
+            $buttons = $params['buttons'];
+
+            if(is_array($buttons)){
+
+                if(count($buttons) > 0){
+
+                    $keyboard['inline_keyboard'] = $buttons;
+
+
+                    $params['reply_markup'] = json_encode($keyboard);
+
+                }
+
+            }
+
+            unset($params['buttons']);
+
+
+        }elseif(array_key_exists('keyboard', $params)){
+
+            $params['reply_markup'] = json_encode(['inline_keyboard' => $params['keyboard']]);
+
+            unset($params['keyboard']);
+
+        }
+
+
+
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $params);
+
+        //execute post
+        $result = curl_exec($ch);
+
+        $result = json_decode($result);
+
+        //close connection
+        curl_close($ch);
+
+        if($result->ok){
+
+            $return = [
+
+                'message_id' => $result->result->message_id
+
+            ];
+
+            return $return;
+
+        }else{
+
+            return false;
+        }
 
     }
+
+}
+
+if (! function_exists('telegramUpdate')) {
+
+    function telegramUpdate($chat_id, $message_id, $message, $params = []){
+
+        $url = 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/editMessageText';
+
+        $params['chat_id'] = $chat_id;
+        $params['message_id'] = $message_id;
+
+        $params['text'] = urldecode($message);
+
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $params);
+
+        //execute post
+        $result = curl_exec($ch);
+
+        $result = json_decode($result);
+
+        //close connection
+        curl_close($ch);
+
+        if($result->ok){
+
+            $return = [
+
+                'message_id' => $result->result->message_id
+
+            ];
+
+            return $return;
+
+        }else{
+
+            return false;
+        }
+
+    }
+
+}
+
+if (! function_exists('telegramAnswerCallback')) {
+
+    function telegramAnswerCallback($callback_id, $message, $params = []){
+
+        $url = 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/answerCallbackQuery';
+
+        $params['callback_query_id'] = $callback_id;
+        $params['text'] = urldecode($message);
+
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $params);
+
+        //execute post
+        $result = curl_exec($ch);
+
+        $result = json_decode($result);
+
+        //close connection
+        curl_close($ch);
+
+        return;
+
+    }
+}
+
+if (! function_exists('telegramDelete')) {
+
+    function telegramDelete($chat_id, $message_id, $params = []){
+
+        $url = 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/deleteMessage';
+
+        $params['chat_id'] = $chat_id;
+        $params['message_id'] = $message_id;
+
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $params);
+
+        //execute post
+        $result = curl_exec($ch);
+
+        $result = json_decode($result);
+
+        //close connection
+        curl_close($ch);
+
+        if($result->ok){
+
+            return true;
+
+        }else{
+
+            return false;
+        }
+
+    }
+
 }
