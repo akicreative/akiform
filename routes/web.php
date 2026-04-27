2697d8d8a8a057dc51b07ab409eacedd3a1fc480
Steven Ishiwara <info@akicreative.net>
2026-04-26 21:14:11 -0400

    feat: convert routes to array syntax for Laravel 10 compatibility

--- routes/web.php
+++ routes/web.php
@@ -1,84 +1,70 @@
 <?php
 
-Route::middleware(['web'])->group(function(){
+use AkiCreative\AkiForms\AssetController;
+use AkiCreative\AkiForms\HomeController;
+use AkiCreative\AkiForms\TelegramController;
+use AkiCreative\AkiForms\DatepickerController;
+use AkiCreative\AkiForms\CmsController;
+use AkiCreative\AkiForms\TextblockController;
+use AkiCreative\AkiForms\UserController;
+use AkiCreative\AkiForms\PageController;
+use AkiCreative\AkiForms\InfolistController;
+use AkiCreative\AkiForms\CategoryController;
 
-	Route::get('/asset/download/{id}/{filename}', 'AkiCreative\AkiForms\AssetController@getpublic')->name('aki.asset.public');
-
-	Route::get('/asset/download/aws/{id}/{fn}', 'AkiCreative\AkiForms\AssetController@aws')->name('aki.asset.aws');
-
-	Route::post('/ajax/akiforms/editor/upload', 'AkiCreative\AkiForms\AssetController@editorupload')->name('aki.editor.upload');
-
-	Route::post('/ajax/akiforms/toast', 'AkiCreative\AkiForms\HomeController@toast')->name('aki.toast');
-
-	Route::get('/telegram/login', 'AkiCreative\AkiForms\TelegramController@login')->name('aki.telegram.login');
-	Route::get('/telegram/register', 'AkiCreative\AkiForms\TelegramController@register')->name('aki.telegram.register');
-	Route::get('/telegram/auth', 'AkiCreative\AkiForms\TelegramController@auth')->name('aki.telegram.auth');
-
+Route::middleware(['web'])->group(function () {
+    Route::get('/asset/download/{id}/{filename}', [AssetController::class, 'getpublic'])->name('aki.asset.public');
+    Route::get('/asset/download/aws/{id}/{fn}', [AssetController::class, 'aws'])->name('aki.asset.aws');
+    Route::post('/ajax/akiforms/editor/upload', [AssetController::class, 'editorupload'])->name('aki.editor.upload');
+    Route::post('/ajax/akiforms/toast', [HomeController::class, 'toast'])->name('aki.toast');
+    Route::get('/telegram/login', [TelegramController::class, 'login'])->name('aki.telegram.login');
+    Route::get('/telegram/register', [TelegramController::class, 'register'])->name('aki.telegram.register');
+    Route::get('/telegram/auth', [TelegramController::class, 'auth'])->name('aki.telegram.auth');
 });
 
-Route::prefix('akiforms')->middleware(['web'])->namespace('AkiCreative\AkiForms')->group(function(){
-
-	Route::get('load/datepicker', 'DatepickerController@index')->name('akidatepicker');
-	Route::post('load/datepicker/calendar', 'DatepickerController@calendar')->name('akidatepickercalendar');
-
+Route::prefix('akiforms')->middleware(['web'])->group(function () {
+    Route::get('load/datepicker', [DatepickerController::class, 'index'])->name('akidatepicker');
+    Route::post('load/datepicker/calendar', [DatepickerController::class, 'calendar'])->name('akidatepickercalendar');
 });
 
+Route::prefix('cms')->middleware(['web', 'isadmin'])->group(function () {
+    Route::get('/', [CmsController::class, 'index'])->name('aki.cms');
 
-Route::prefix('cms')->middleware(['web', 'isadmin'])->namespace('AkiCreative\AkiForms')->group(function(){
+    Route::get('/textblocks', [TextblockController::class, 'index'])->name('aki.textblock.index');
+    Route::get('/textblocks/create', [TextblockController::class, 'create'])->name('aki.textblock.create');
+    Route::post('/textblocks/store', [TextblockController::class, 'store'])->name('aki.textblock.store');
+    Route::get('/textblocks/{id}/edit', [TextblockController::class, 'edit'])->name('aki.textblock.edit');
+    Route::post('/textblocks/{id}/update', [TextblockController::class, 'update'])->name('aki.textblock.update');
 
-	Route::get('/', 'CmsController@index')->name('aki.cms');
+    Route::get('/users', [UserController::class, 'index'])->name('aki.users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('aki.users.create');
+    Route::post('/users/store', [UserController::class, 'store'])->name('aki.users.store');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('aki.users.edit');
+    Route::post('/users/{id}/update', [UserController::class, 'update'])->name('aki.users.update');
 
-	Route::get('/textblocks', 'TextblockController@index')->name('aki.textblock.index');
-	Route::get('/textblocks/create', 'TextblockController@create')->name('aki.textblock.create');
-	Route::post('/textblocks/store', 'TextblockController@store')->name('aki.textblock.store');
-	Route::get('/textblocks/{id}/edit', 'TextblockController@edit')->name('aki.textblock.edit');
-	Route::post('/textblocks/{id}/update', 'TextblockController@update')->name('aki.textblock.update');
+    Route::get('/pages', [PageController::class, 'index'])->name('aki.page.index');
+    Route::get('/pages/create', [PageController::class, 'create'])->name('aki.page.create');
+    Route::post('/pages/store', [PageController::class, 'store'])->name('aki.page.store');
+    Route::get('/pages/{id}/edit', [PageController::class, 'edit'])->name('aki.page.edit');
+    Route::post('/pages/{id}/update', [PageController::class, 'update'])->name('aki.page.update');
 
-	Route::get('/users', 'UserController@index')->name('aki.users.index');
-	Route::get('/users/create', 'UserController@create')->name('aki.users.create');
-	Route::post('/users/store', 'UserController@store')->name('aki.users.store');
-	Route::get('/users/{id}/edit', 'UserController@edit')->name('aki.users.edit');
-	Route::post('/users/{id}/update', 'UserController@update')->name('aki.users.update');
+    Route::get('/assets', [AssetController::class, 'index'])->name('aki.asset.index');
+    Route::post('/assets', [AssetController::class, 'index']);
+    Route::get('/assets/create', [AssetController::class, 'create'])->name('aki.asset.create');
+    Route::post('/assets/store', [AssetController::class, 'store'])->name('aki.asset.store');
+    Route::get('/assets/{id}/edit', [AssetController::class, 'edit'])->name('aki.asset.edit');
+    Route::post('/assets/{id}/update', [AssetController::class, 'update'])->name('aki.asset.update');
+    Route::post('/assets/{id}/destroy', [AssetController::class, 'destroy'])->name('aki.asset.destroy');
 
-	Route::get('/pages', 'PageController@index')->name('aki.page.index');
-	Route::get('/pages/create', 'PageController@create')->name('aki.page.create');
-	Route::post('/pages/store', 'PageController@store')->name('aki.page.store');
-	Route::get('/pages/{id}/edit', 'PageController@edit')->name('aki.page.edit');
-	Route::post('/pages/{id}/update', 'PageController@update')->name('aki.page.update');
+    Route::get('/categories', [CategoryController::class, 'index'])->name('aki.categories.index');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('aki.categories.create');
+    Route::post('/categories/store', [CategoryController::class, 'store'])->name('aki.categories.store');
+    Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('aki.categories.edit');
+    Route::post('/categories/{id}/update', [CategoryController::class, 'update'])->name('aki.categories.update');
 
-	Route::get('/assets', 'AssetController@index')->name('aki.asset.index');
-	Route::post('/assets', 'AssetController@index');
-	Route::get('/assets/create', 'AssetController@create')->name('aki.asset.create');
-	Route::post('/assets/store', 'AssetController@store')->name('aki.asset.store');
-	Route::get('/assets/{id}/edit', 'AssetController@edit')->name('aki.asset.edit');
-	Route::post('/assets/{id}/update', 'AssetController@update')->name('aki.asset.update');
-	Route::post('/assets/{id}/destroy', 'AssetController@destroy')->name('aki.asset.destroy');
-
-	/*
-
-	Route::get('/assets/{category?}', 'AssetController@index')->name('aki.asset.category.index');
-	Route::post('/assets/{category?}', 'AssetController@index')->name('aki.asset.category.index');
-	Route::get('/assets/{category}/create', 'AssetController@create')->name('aki.asset.category.create');
-	Route::post('/assets/{category}/store', 'AssetController@store')->name('aki.asset.category.store');
-	Route::get('/assets/{id}/{category}/edit', 'AssetController@edit')->name('aki.asset.category.edit');
-	Route::post('/assets/{id}/{category}/update', 'AssetController@update')->name('aki.asset.category.update');
-	Route::post('/assets/{id}/{category}/destroy', 'AssetController@destroy')->name('aki.asset.category.destroy');
-
-	*/
-
-	Route::get('/categories', 'CategoryController@index')->name('aki.categories.index');
-	Route::get('/categories/create', 'CategoryController@create')->name('aki.categories.create');
-	Route::post('/categories/store', 'CategoryController@store')->name('aki.categories.store');
-	Route::get('/categories/{id}/edit', 'CategoryController@edit')->name('aki.categories.edit');
-	Route::post('/categories/{id}/update', 'CategoryController@update')->name('aki.categories.update');
-
-	Route::get('/lists', 'InfolistController@index')->name('aki.lists.index');
-	Route::post('/lists/store', 'InfolistController@store')->name('aki.lists.store');
-	Route::get('/lists/{id}', 'InfolistController@edit')->name('aki.lists.edit');
-	Route::post('/lists/{id}/update', 'InfolistController@update')->name('aki.lists.save');
-	Route::post('/lists/orderby', 'InfolistController@orderby')->name('aki.lists.orderby');
-	Route::post('/lists/{id}/destroy', 'InfolistController@destroy')->name('aki.lists.destroy');
-
-
+    Route::get('/lists', [InfolistController::class, 'index'])->name('aki.lists.index');
+    Route::post('/lists/store', [InfolistController::class, 'store'])->name('aki.lists.store');
+    Route::get('/lists/{id}', [InfolistController::class, 'edit'])->name('aki.lists.edit');
+    Route::post('/lists/{id}/update', [InfolistController::class, 'update'])->name('aki.lists.save');
+    Route::post('/lists/orderby', [InfolistController::class, 'orderby'])->name('aki.lists.orderby');
+    Route::post('/lists/{id}/destroy', [InfolistController::class, 'destroy'])->name('aki.lists.destroy');
 });
-
